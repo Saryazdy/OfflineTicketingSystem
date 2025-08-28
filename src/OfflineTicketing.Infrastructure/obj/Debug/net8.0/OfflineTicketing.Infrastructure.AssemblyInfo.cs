8e3a0abdeea59b0dca24e196654b5813bbef69af
sadeghsaryazdy <sadeghsaryazdy71@gmail.com>
2025-08-29 03:11:30 +0330

    resolve error

--- src/OfflineTicketing.Infrastructure/obj/Debug/net8.0/OfflineTicketing.Infrastructure.AssemblyInfo.cs
+++ src/OfflineTicketing.Infrastructure/obj/Debug/net8.0/OfflineTicketing.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OfflineTicketing.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b286f6bfb74e9bb6b0ae9ae455993423c96af109")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88990a5e98e7a177c01d1129c2b8e14255bc410")]
 [assembly: System.Reflection.AssemblyProductAttribute("OfflineTicketing.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OfflineTicketing.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
